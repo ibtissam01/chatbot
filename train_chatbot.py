ee1a02b6db0022425d12e8588ba844cd7ffa0f34
Ibtissam Labyady <89752387+ibtissam01@users.noreply.github.com>
2023-12-10 19:56:33 +0100

    Update train_chatbot.py

--- train_chatbot.py
+++ train_chatbot.py
@@ -77,8 +77,6 @@ sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-model.save('chatbot_model.h5', hist)
-
 pickle.dump(hist.history, open('hist.pkl', 'wb'))
 
 print("model created")
