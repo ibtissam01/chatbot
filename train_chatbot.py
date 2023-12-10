f4dba654e2bf6eacbba9271493c7efe28f71475b
Ibtissam Labyady <89752387+ibtissam01@users.noreply.github.com>
2023-12-10 21:41:06 +0100

    Update train_chatbot.py

--- train_chatbot.py
+++ train_chatbot.py
@@ -1,17 +1,19 @@
 import nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+
+import nltk
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-from transformers import GPT2Tokenizer, GPT2LMHeadModel
+
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 import random
-import pickle
-nltk.download('punkt')
-nltk.download('wordnet')
+
 words=[]
 classes = []
 documents = []
@@ -37,13 +39,13 @@ words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words
 words = sorted(list(set(words)))
 
 classes = sorted(list(set(classes)))
-print(len(documents), "documents")
-print(len(classes), "classes", classes)
-print(len(words), "unique lemmatized words", words)
+print (len(documents), "documents")
+print (len(classes), "classes", classes)
+print (len(words), "unique lemmatized words", words)
 
 
-pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(classes, open('classes.pkl', 'wb'))
+pickle.dump(words,open('words.pkl','wb'))
+pickle.dump(classes,open('classes.pkl','wb'))
 
 training = []
 output_empty = [0] * len(classes)
@@ -64,9 +66,21 @@ train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
 
-# Charger le tokenizer et le modèle GPT-2 pré-entraîné
-tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-model = GPT2LMHeadModel.from_pretrained('gpt2')
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+
+pickle.dump(hist.history, open('hist.pkl', 'wb'))
 
 print("model created")
 
