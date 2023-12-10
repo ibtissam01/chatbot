65c5abf5861891022f95277dd2ad23b5442d82b8
Ibtissam Labyady <89752387+ibtissam01@users.noreply.github.com>
2023-12-10 20:27:35 +0100

    Update train_chatbot.py

--- train_chatbot.py
+++ train_chatbot.py
@@ -3,13 +3,13 @@ from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 import json
 import pickle
-
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 import random
-
+import pickle
 nltk.download('punkt')
 nltk.download('wordnet')
 words=[]
@@ -64,20 +64,9 @@ train_x = list(training[:,0])
 train_y = list(training[:,1])
 print("Training data created")
 
-model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(32, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(len(train_y[0]), activation='softmax'))
-
-sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
-
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
-pickle.dump(hist.history, open('hist.pkl', 'wb'))
+# Charger le tokenizer et le modèle GPT-2 pré-entraîné
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 print("model created")
 
