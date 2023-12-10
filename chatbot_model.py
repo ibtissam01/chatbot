c157836119a8653d89872865e53072f3467e7d8d
Ibtissam Labyady <89752387+ibtissam01@users.noreply.github.com>
2023-12-10 17:59:46 +0100

    Update chatbot_model.py

--- chatbot_model.py
+++ chatbot_model.py
@@ -4,7 +4,7 @@ lemmatizer = WordNetLemmatizer()
 import pickle
 import numpy as np
 
-from keras.models import load_model
+from tensorflow.keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
 import random
