7dd5271d38bab64ceab0a7735c798b5429215d51
Ibtissam Labyady <89752387+ibtissam01@users.noreply.github.com>
2023-12-10 21:43:01 +0100

    Update train_chatbot.py

--- train_chatbot.py
+++ train_chatbot.py
@@ -72,7 +72,10 @@ model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
+
 
 sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
